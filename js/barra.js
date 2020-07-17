76576263ecefb10ff6cf9509654b2a180c585f51
vidal987 <mvsouza.af@gmail.com>
2020-07-17 18:30:51 -0300

    alterações gerais

--- js/barra.js
+++ js/barra.js
@@ -17,45 +17,72 @@ function criarGrafico(idGrafico, dados, cor){
   options: {}
   });
 }
+
+
 let Dados = [{
   "id": 1,
   "Temperatura": 109.0,
-  "Marca": "Pontiac"
+  "Mes": "Janeiro"
 }, {
   "id": 2,
   "Temperatura": 105.0,
-  "Marca": "Mercedes-Benz"
+  "Mes": "Fevereiro"
 }, {
   "id": 3,
-  "Temperatura": 90.5,
-  "Marca": "Jeep"
+  "Temperatura": 101.5,
+  "Mes": "Março"
 }, {
   "id": 4,
   "Temperatura": 106.5,
-  "Marca": "Toyota"
+  "Mes": "Abril"
 }, {
   "id": 5,
-  "Temperatura": 91.0,
-  "Marca": "Volkswagen"
+  "Temperatura": 111.0,
+  "Mes": "Maio"
 }, {
   "id": 6,
   "Temperatura": 100.5,
-  "Marca": "Audi"
+  "Mes": "Junho"
 }]
 
-let Marc = Dados.map( value => {return value.Marca} );
+var Dados2 = [{
+  "Temperatura": 100.5,
+  "Mes": "Janeiro"
+}, {
+  "Temperatura": 110.0,
+  "Mes": "Fevereiro"
+}, {
+  "Temperatura": 90.5,
+  "Mes": "Março"
+}, {
+  "Temperatura": 102.5,
+  "Mes": "Abril"
+}, {
+  "Temperatura": 119.0,
+  "Mes": "Maio"
+}, {
+  "Temperatura": 120.5,
+  "Mes": "Junho"
+}]
+let Marc = Dados.map( value => {return value.Mes} );
 let Temp = Dados.map(value => {return value.Temperatura})
 
+var Mes = Dados2.map(value => {return value.Mes})
+var celcius = Dados2.map(value => {return value.Temperatura})
+
 var dadosGrafico1 = {
-  "indicador": "Temperatura °C",
+  "indicador": "Temperatura °C Por 6 Meses de um Fusca",
   "RótuloX": Marc,
   "dados": Temp
 
 }
 
+var dadosGrafico2 = {
+  "indicador": "Temperatura °C Por 6 Meses de um Chevette",
+  "RótuloX": Mes,
+  "dados": celcius
+
+}
+
 criarGrafico("grafico-1", dadosGrafico1, 'rgb(228, 63, 90')
-criarGrafico("grafico-2", dadosGrafico1, 'rgb(228, 63, 90')
-criarGrafico("grafico-3", dadosGrafico1, 'rgb(228, 63, 90')
-criarGrafico("grafico-4", dadosGrafico1, 'rgb(228, 63, 90')
-criarGrafico("grafico-5", dadosGrafico1, 'rgb(228, 63, 90')
-criarGrafico("grafico-6", dadosGrafico1, 'rgb(228, 63, 90')
+criarGrafico("grafico-2", dadosGrafico2, 'rgb(228, 63, 90')
